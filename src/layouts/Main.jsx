3c6e8376dd211608e7d3b37c03ca6cf685e4c869
monircom <monircom@gmail.com>
2024-05-13 21:43:30 +0600

    routes added

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom'
+//import Navbar from '../components/Navbar'
+//import Footer from '../components/Footer'
+
+const Main = () => {
+  return (
+    <div>
+       <h1>Main</h1>
+      {/* Navbar */}
+      {/* <Navbar /> */}
+      {/* Outlet */}
+      <div className='min-h-[calc(100vh-306px)]'>
+        {/* <Outlet /> */}
+      </div>
+      {/* Footer */}
+      {/* <Footer /> */}
+    </div>
+  )
+}
+
+export default Main
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import router from './routes/Routes'
 import './index.css'
+import { Router, RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 className='font-freeman'> Welcome to Volunteer</h1>
-  </React.StrictMode>,
+    <RouterProvider router = { router }   />
+   </React.StrictMode>,
 )
