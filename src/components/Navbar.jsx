899f0075b0c37c07d433837719f6bcecb0976d3d
monircom <monircom@gmail.com>
2024-05-13 22:29:36 +0600

    login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,46 @@
 import logo from '../assets/logo2.png'
+import { useContext } from 'react'
+//import logo from '../assets/images/logo.png'
+import { AuthContext } from '../provider/AuthProvider'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-    return (
-      <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
-        <div className='flex-1'>
-          <div className='flex gap-2 items-center'>
-            <img className='w-auto h-7' src={logo} alt='' />
-            <span className='font-bold'>Volunteer Voyage</span>
-          </div>
-        </div>
-        <div className='flex-none'>
-          <ul className='menu menu-horizontal px-1'>
+  const { user, logOut } = useContext(AuthContext)
+  return (
+    <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
+      <div className='flex-1'>
+        <Link to='/' className='flex gap-2 items-center'>
+          <img className='w-auto h-7' src={logo} alt='' />
+          <span className='font-bold'>SoloSphere</span>
+        </Link>
+      </div>
+      <div className='flex-none'>
+        <ul className='menu menu-horizontal px-1'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/jobs'>All Jobs</Link>
+          </li>
+
+          {!user && (
             <li>
-              <div>Home</div>
+              <Link to='/login'>Login</Link>
             </li>
-  
-            <li>
-              <div>Login</div>
-            </li>
-          </ul>
-  
+          )}
+        </ul>
+
+        {user && (
           <div className='dropdown dropdown-end z-50'>
             <div
               tabIndex={0}
               role='button'
               className='btn btn-ghost btn-circle avatar'
             >
-              <div className='w-10 rounded-full' title=''>
+              <div title={user?.displayName} className='w-10 rounded-full'>
                 <img
                   referrerPolicy='no-referrer'
                   alt='User Profile Photo'
-                  src=''
+                  src={user?.photoURL}
                 />
               </div>
             </div>
@@ -38,25 +49,33 @@ const Navbar = () => {
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
             >
               <li>
-                <div className='justify-between'>Add Job</div>
+                <Link to='/add-job' className='justify-between'>
+                  Add Job
+                </Link>
               </li>
               <li>
-                <div>My Posted Jobs</div>
+                <Link to='/my-posted-jobs'>My Posted Jobs</Link>
               </li>
               <li>
-                <div>My Bids</div>
+                <Link to='/my-bids'>My Bids</Link>
               </li>
               <li>
-                <div>Bid Requests</div>
+                <Link to='/bid-requests'>Bid Requests</Link>
               </li>
               <li className='mt-2'>
-                <button className='bg-gray-200 block text-center'>Logout</button>
+                <button
+                  onClick={logOut}
+                  className='bg-gray-200 block text-center'
+                >
+                  Logout
+                </button>
               </li>
             </ul>
           </div>
-        </div>
+        )}
       </div>
-    )
-  }
-  
-  export default Navbar
+    </div>
+  )
+}
+
+export default Navbar
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import Home from "../pages/Home";
+import Login from './../pages/Authentication/Login';
+import Register from './../pages/Authentication/Register';
 
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <Main />,
+        children:[
+            {
+                index: true,
+                element: <Home></Home>    
 
-        element: <Main />
+            },
+            {
+                path: '/login',
+                element: <Login></Login>    
+
+            },
+
+            {
+                path: '/register',
+                element: <Register></Register>    
+
+            },
+
+
+        ]
     }
 ])
 
